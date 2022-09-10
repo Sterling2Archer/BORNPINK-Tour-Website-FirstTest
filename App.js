59c3347f35ebeebfcd1f9a5bbd8f6f65fb1b183f
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2022-09-09 21:14:39 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App';
+import './App.css';
+import Home from './components/pages/Home';
+import Music from './components/pages/Music';
+import MembersOfBLACKPINK from './components/pages/MembersOfBLACKPINK';
+import SignUpForEmailUpdates from './components/pages/SignUpForEmailUpdates';
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
        <Router>
          <Navbar />
          <Routes> 
-           <Route path='/' exact />
+         <Route path='/' exact component={Home}/>
+         <Route path='/music' component={Music}/>
+         <Route path='membersofblackpink' component={MembersOfBLACKPINK}/>
+         <Route path='signupforemailupdates' component={SignUpForEmailUpdates}/>
          </Routes>
        </Router>
     </>
