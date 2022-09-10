72ff794c857d2f0571a01b314a9b3d70ee94326d
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2022-09-10 19:52:51 -0400

    Update HeroSection.js

--- HeroSection.js
+++ HeroSection.js
@@ -2,34 +2,31 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-import { Link } from 'react-router-dom';
-
 
 function HeroSection() {
   return (
-   <div className='hero-container'>
-   <Link>
-    <center><img src="./images2/kisspink.jpg" alt="BLACKPINK"/></center> </Link>
-      <h1>BORN PINK TOUR: SEOUL</h1>
-      <p>A MESSAGE FROM BLACKPINK</p>
+    <div className='hero-container'>
+     <video autoPlay loop muted src="./videos/tourmessage.mp4"/>
+      <h1>BLACKPINK BORN PINK TOUR</h1>
+      <p>TOUR 2023</p>
       <div className='hero-btns'>
-        <Button 
-          className='btns' 
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+        <Button
+        className='btns'
+        buttonStyle='btn--outline'
+        buttonSize='btn--large'
         >
-          GET STARTED 
+          GET STARTED
         </Button>
         <Button
-          className='btns' 
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
+        className='btns'
+        buttonStyle='btn--primary'
+        buttonSize='btn--large'
         >
-          GET STARTED <i className='far fa-play-circle' />
-        </Button> 
+          CLICK HERE <i className='far
+          fa-play-circle' />
+        </Button>
       </div>
     </div>
   )
 }
-
 export default HeroSection;
